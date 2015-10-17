70f890b5a59d3d5570616be22c42019b430bbdeb
Adrianne Diaz <adrianne.programmer@gmail.com>
2015-10-17 19:44:10 -0400

    Merged again

--- lib/librisk.cpp
+++ lib/librisk.cpp
@@ -3,13 +3,11 @@ using namespace std;
 
 void librisk_print_todo() {
 	cout << "TODO\n";
-<<<<<<< HEAD
+
     int s = 5;
     int *p = &s;
     *p = 45;
     cout << p << "test";
 }
 
-=======
-}
->>>>>>> a53aafe9d2f22e3aa9152d712d1290b5f75b6802
+
