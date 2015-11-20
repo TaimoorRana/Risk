ec487f1492a9fa984bde42c36af7d73b4905d25c
Taimoor <taimoorrana1@hotmail.com>
2015-11-20 15:42:44 -0500

    fixed bug where defender had an advantage over the attacker

--- lib/librisk.cpp
+++ lib/librisk.cpp
@@ -27,6 +27,6 @@ std::vector<int> rollDice(int amount)
 		randNumber = (randNumber % 6) + 1;
 		results.push_back(randNumber);
 	}
-    std::sort(results.begin(), results.end());
+	std::sort(results.rbegin(), results.rend());
 	return results;
 }
